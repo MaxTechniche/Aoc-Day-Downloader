6405bbee37ce484372856d67baccee08d5048ae0
MaxTechniche <Max.Techniche@gmail.com>
2020-12-08 18:38:28 -0700

    makefile and code to create day structure in whatever year

--- make_day.py
+++ make_day.py
@@ -1,19 +1,50 @@
 import os
 
 
-def day_valid(day):
-    pass
+def vac_year(year):
+    if str(year) not in os.listdir():
+        print(f"Year {year} not found.")
+        while True:
+            create = input(f"Would you like to create year {year}? [y]/n") or "y"
+            if create == "y":
+                print("Creating year {year}...")
+                os.mkdir(str(year))
+                print("Done")
+                break
+            elif create == "n":
+                print("Exiting...")
+                exit()
+            else:
+                print(f"{create} not a valid command")
+
+    print(f"Entering year {year}")
+    os.chdir(str(year))
+
+
+def vac_day(day, year):
+    day = "Day_" + str(day).zfill(2)
+
+    if day not in os.listdir():
+        print(f"Creating {day}")
+        os.mkdir(day)
+        print(f"Done")
+        os.chdir(day)
+
+    else:
+        print(f"{day} already in {year}")
+        print("Exiting...")
+        exit()
 
 
 while True:
     year = input("Year [2020]: ") or 2020
     try:
         int(year)
+        vac_year(year)
         break
     except ValueError:
         print("Unable to convert year to integer. Try again.")
 
-print(f"Year: {year}")
 
 while True:
     day = input("Day number: ")
@@ -21,19 +52,24 @@ while True:
         if int(day) > 25 or int(day) < 1:
             print("Number is not in range (1-25)")
             continue
-        if day_valid(day):
-            break
+        vac_day(day, year)
+        break
     except ValueError:
         print("Unable to convert day to integer. Try again.")
 
-    # do = input(f"Day {day}? [y]/n/q :") or "y"
-    # if do == "q":
-    #     print("Exiting...")
-    #     exit()
-    # elif do == "n":
-    #     continue
-    # elif do == "y" or do == "":
-    #     print(f"Creating day {day}...")
-    #     break
+day = "Day_" + str(day).zfill(2)
 
-print(f"Day: {day}")
+open("question", "w")
+open("input", "w")
+with open("solution.py", "w") as solution:
+    solution.write(
+        f"""from time import time
+
+t1 = time()
+
+with open("{year}/{day}/input") as f:
+    lines = f.read().splitlines()
+    
+print("Time:", time() - t1)
+"""
+    )

37e337a1c8bdffd210bfe7090e60102faf04f19a
Jacob Maxfield <maxtechniche@Jacobs-MacBook-Pro.local>
2021-12-10 13:36:30 -0700

    Day 10

---
+++ 2021/Day_10/solution.py
@@ -0,0 +1,51 @@
+from time import time
+
+t1 = time()
+
+with open("2021/Day_10/input") as f:
+    lines = f.read().splitlines()
+
+
+total_syntax_score = 0
+
+part_1_scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+
+completion_scores = []
+
+part_2_scores = {')': 1, ']': 2, '}': 3, '>': 4}
+
+# Go through the lines
+# for each line, go through each character
+# keep track of open brackets
+# if closing bracket doesn't match, then the line is corrupted
+
+brackets = {'(': ')', '[': ']', '{': '}', '<': '>'}
+
+for line in lines:
+    open_brackets = [line[0]]
+
+    for bracket in line[1:]:
+        if bracket in brackets.keys():
+            open_brackets.append(bracket)
+        else:
+            last_open_bracket = open_brackets[-1]
+            if brackets[last_open_bracket] != bracket:
+                total_syntax_score += part_1_scores[bracket]
+                break
+            else:
+                open_brackets = open_brackets[:-1]
+    else:
+        # Part 2
+        score = 0
+        for bracket in open_brackets[::-1]:
+            score *= 5
+            score += part_2_scores[brackets[bracket]]
+        completion_scores.append(score)
+    
+
+print("Part 1: " + str(total_syntax_score))
+
+completion_scores.sort()
+print("Part 2: " + str(completion_scores[len(completion_scores)//2]))
+
+print("Time:", time() - t1)

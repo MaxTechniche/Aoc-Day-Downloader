9ba98193ee701c58062c404b07bf353da5e80349
Jacob <max.techniche@gmail.com>
2022-11-25 15:23:02 -0600

    Working on webapp to download

---
+++ tools/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Advent of Code Day Downloader")
+st.write("## This app retrieves the question and/or input for the selected day(s) of _`Advent of Code`_")
+
+st.download_button("_Download!_")
